3ba34dca08b8e70e1067998fa7ad54ba555efb3b
Bradon Dundas <sovietworkwaffle@gmail.com>
2018-06-13 15:02:20 -0600

    Added documentation and fixed a stupd typo

--- bot.js
+++ bot.js
@@ -11,17 +11,15 @@ client.on('message', msg => {
   {if (msg.content === 'i+') {
     request('http://inspirobot.me/api?generate=true', function (error, response, body) {
         msg.channel.send(body); 
-    });
+    });//regular quotes
   }else if (msg.content === 'ix+'){
     request('http://inspirobot.me/api?generate=true&season=xmas', function (error, response, body) {
       msg.channel.send(body); 
-    });
+    });//xmas themed quotes
   }}});
 client.on('message', msg =>{
   {if (msg.content === 'info+'){
     msg.channel.send("Hi there! I'm Inspirobotbot (not actually licensed). Just type i+ for a regular example or if you're feeling festive just type ix+ !")
-  }}
-}
-    );
+  }}});//info panel to explain the bot
 
 client.login(config.token);
